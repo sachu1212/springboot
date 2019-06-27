5753a42c3463a3ee8f7884f64d95102d1624a6dd
sachu1212 <sachubsingh@gmail.com>
2019-06-27 11:42:24 +0100

    working application

--- src/main/java/qa/nationwide1/controllers/HomeController.java
+++ src/main/java/qa/nationwide1/controllers/HomeController.java
@@ -1,10 +1,11 @@
-package controller;
+package qa.nationwide1.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	
 	@RequestMapping("/") //this annotation is for specifying the url that will trigger this method
 	public String home() {
 		return "hello everyone";
