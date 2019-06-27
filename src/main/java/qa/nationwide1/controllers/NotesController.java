155457cc30bf172f70589149deab20295eac19e1
sachu1212 <sachubsingh@gmail.com>
2019-06-27 14:45:51 +0100

    final commit

--- src/main/java/qa/nationwide1/controllers/NotesController.java
+++ src/main/java/qa/nationwide1/controllers/NotesController.java
@@ -3,6 +3,8 @@ package qa.nationwide1.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,22 @@ public class NotesController {
 	public List<Note> list(){
 		return notesRepository.findAll();
 	}
+	
+	@RequestMapping(value = "notes", method = RequestMethod.POST)
+	public Note create(@RequestBody Note note) {
+		return notesRepository.saveAndFlush(note);
+	}
+	
+	@RequestMapping(value = "notes/{id}", method = RequestMethod.GET)
+	public Note getNote(@PathVariable Long id) {
+		return notesRepository.findOne(id);
+	}
 
+	@RequestMapping(value = "notes/{id}", method = RequestMethod.DELETE)
+	public Note delete(@PathVariable Long id) {
+		Note existingNote = notesRepository.findOne(id);
+		notesRepository.delete(existingNote);
+			return existingNote;
+	}
+	
 }
